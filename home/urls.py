54f2c0d28631573fb843d5f7b30cb6a478c02f40
Flavio-Lima18 <fplj2005@gmail.com>
2024-10-26 23:15:05 -0300

    venv

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+# home/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('sobre', views.sobre, name='sobre'), 
+    path('contato', views.contato, name='contato'),
+    path('ajuda', views.ajuda, name='ajuda'),
+]
