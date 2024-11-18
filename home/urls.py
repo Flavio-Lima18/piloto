a85d9c6bb1540cf7c14a07776a39d7fbc7eafdad
Flavio-Lima18 <fplj2005@gmail.com>
2024-11-18 01:18:10 -0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path # type: ignore
 from .views import index, sobre, contato
+from . import views
 
 urlpatterns = [
     path('', index, name='index'),
     path('sobre/', sobre, name='sobre'),
     path('contato/', contato, name='entrar_em_contato'),  
+    path('item/<int:id>/', views.exibir_item, name='exibir_item'),
+    path('perfil/<str:usuario>/', views.perfil, name='perfil'),
 ]
