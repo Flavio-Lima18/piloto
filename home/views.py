31c857893b11d37193eefa7227a77a517c353cdc
Flavio-Lima18 <fplj2005@gmail.com>
2024-11-18 01:18:14 -0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
 def index(request):
     return render(request, "index.html")
@@ -8,3 +8,11 @@ def sobre(request):
 
 def contato(request):
     return render(request, "contato.html")
+
+#view exibir_item
+def exibir_item(request, id):
+    return render(request, "exibir_item.html", {'id':id})
+#view perfil
+def perfil(request, usuario):
+    return render(request, "perfil.html", {'usuario':usuario})
+
