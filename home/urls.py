4c9c95830a0212b49ed0e0ec316bd3c6e3d72e9a
Flavio-Lima18 <fplj2005@gmail.com>
2024-11-24 22:29:21 -0300

    Atualizações

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('contato/', contato, name='entrar_em_contato'),  
     path('item/<int:id>/', views.exibir_item, name='exibir_item'),
     path('perfil/<str:usuario>/', views.perfil, name='perfil'),
+    path('dia_da_semana/<int:numero>/', views.dia_da_semana, name='dia_da_semana'),
 ]
--- home/views.py
+++ home/views.py
@@ -16,3 +16,15 @@ def exibir_item(request, id):
 def perfil(request, usuario):
     return render(request, "perfil.html", {'usuario':usuario})
 
+def dia_da_semana(request, numero):
+    dias = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado"
+    }
+    dia = dias.get(numero, "Dia inválido")
+    return render(request, "dia_da_semana.html", {'numero': numero, 'dia': dia})
