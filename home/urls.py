93adcfbaf31555a9a17a7f1c2424850a58a862e4
Flavio-Lima18 <fplj2005@gmail.com>
2024-11-25 21:10:17 -0300

    Atualizações

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path # type: ignore
-from .views import index, sobre, contato
 from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('sobre/', sobre, name='sobre'),
-    path('contato/', contato, name='entrar_em_contato'),  
-    path('item/<int:id>/', views.exibir_item, name='exibir_item'),
-    path('perfil/<str:usuario>/', views.perfil, name='perfil'),
-    path('dia_da_semana/<int:numero>/', views.dia_da_semana, name='dia_da_semana'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 ]
+
+    # path('', views.index, name='index'),
+    # path('sobre/', views.sobre, name='sobre'),
+    # path('contato/', views.contato, name='entrar_em_contato'),  
+    # path('item/<int:id>/', views.exibir_item, name='exibir_item'),
+    # path('perfil/<str:usuario>/', views.perfil, name='perfil'),
+    # path('dia_da_semana/<int:numero>/', views.dia_da_semana, name='dia_da_semana'),
+
